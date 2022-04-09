a99810c5a3fc09d4cdf41ed15dcbedbcfccaf0ab
Thom Castermans <thom.castermans@ordina.nl>
2022-04-09 18:04:39 +0200

    Implement initial parsing logic
    Make the path to the input file a parameter, with a default value from
    the `application.properties` that is set to an ignored file in the test
    resources for now. Plan is to add anonymized input files to the test
    resources in a later stage.

---
+++ src/main/java/nl/denvelop/stenokhoria/PlutusCommand.java
@@ -0,0 +1,77 @@
+package nl.denvelop.stenokhoria;
+
+import static com.prowidesoftware.swift.model.field.Field61.AMOUNT;
+import static com.prowidesoftware.swift.model.field.Field61.IDENTIFICATION_CODE;
+import static com.prowidesoftware.swift.model.field.Field61.REFERENCE_FOR_THE_ACCOUNT_OWNER;
+import static com.prowidesoftware.swift.model.field.Field61.TRANSACTION_TYPE;
+import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
+
+import com.prowidesoftware.swift.model.SwiftBlock4;
+import com.prowidesoftware.swift.model.field.Field61;
+import com.prowidesoftware.swift.model.mt.mt9xx.MT940;
+import java.io.File;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.IDefaultValueProvider;
+import picocli.CommandLine.Model.ArgSpec;
+import picocli.CommandLine.Parameters;
+
+@Slf4j
+@Command(name = "plutus", mixinStandardHelpOptions = true,
+    defaultValueProvider = PlutusCommand.DefaultValueProvider.class)
+public class PlutusCommand implements Runnable {
+
+    @Parameters(paramLabel = "<file>", description = "Path to input MT940 file to parse.")
+    private String path;
+
+    @Override
+    public void run() {
+        val inputFile = new File(path);
+        if (!inputFile.exists() || !inputFile.canRead()) {
+            log.error("Cannot open '%s' for reading, does it exist?".formatted(path));
+            return;
+        }
+
+        final SwiftBlock4 block4;
+        try {
+            block4 = MT940.parse(inputFile).getSwiftMessage().getBlock4();
+        } catch (IOException ioe) {
+            log.error(ioe.getMessage(), ioe);
+            return;
+        }
+
+        val loop = block4.getSubBlock(
+            block4.getTagByNumber(60), block4.getTagByNumber(62));
+        log.info("Parsed %d transactions".formatted(loop.size()));
+        for (var i = 0; i < loop.size(); ++i) {
+            val tag = loop.getTag(i);
+            if (tag.getNameAsInt() == 61) {
+                val field = (Field61) tag.asField();
+                log.info("-----");
+                log.info("amount = %s".formatted(field.getComponent(AMOUNT)));
+                log.info("transaction type = %s".formatted(field.getComponent(TRANSACTION_TYPE)));
+                log.info("ID code = %s".formatted(field.getComponent(IDENTIFICATION_CODE)));
+                log.info("from = %s".formatted(field.getComponent(REFERENCE_FOR_THE_ACCOUNT_OWNER)));
+                if (i + 1 < loop.size() && loop.getTag(i + 1).getNameAsInt() == 86) {
+                    log.info("description = %s".formatted(loop.getTag(i + 1).getValue()));
+                    i++;
+                }
+            }
+        }
+    }
+
+    protected static class DefaultValueProvider implements IDefaultValueProvider {
+
+        @Override
+        public String defaultValue(final ArgSpec argSpec) {
+            if ("<file>".equals(argSpec.paramLabel())) {
+                return getConfig().getValue("input.path", String.class);
+            }
+            return null;
+        }
+
+    }
+
+}
